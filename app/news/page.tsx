4af928b6ef83ba0f7ed31fd9c6fa1ffa4f90bc36
gabyballester <gaby.ballester@gmail.com>
2024-07-09 11:33:20 +0200

    Add news list to news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 
-const links = [
-  { href: "/news/first-news", text: "First News Item" },
-  { href: "/news/second-news", text: "Second News Item" },
-  { href: "/news/third-news", text: "Third News Item" },
-];
+import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsPage() {
   return (
-    <div>
+    <>
       <h1>News Page</h1>
       <ul className="news-list">
-        {links.map(({ href, text }) => (
-          <li key={href}>
-            <Link href={href}>{text}</Link>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
---
+++ types.d.ts
@@ -0,0 +1,8 @@
+export type DummyNewType = {
+  id: string;
+  slug: string;
+  title: string;
+  image: string;
+  date: string;
+  content: string;
+};
