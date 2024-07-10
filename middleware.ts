3a74b94ecfed7309130951b28ca61cab7b1b80ed
gabyballester <gaby.ballester@gmail.com>
2024-07-10 03:14:20 +0200

    Add dummy backend

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // return NextResponse.redirect();
+  // return Nextfunction GETResponse.redirect();
   return NextResponse.next();
 }
 
